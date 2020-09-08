b01e3bdc97f770683aee88a30f9283ed73de3c41
dbc2201 <divyanshb22@gmail.com>
2020-09-09 00:32:54 +0530

    create file UncaughtException.java

---
+++ src/exceptions/UncaughtException.java
@@ -0,0 +1,10 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 09/09/20
+ *  Time: 12:32 AM
+ *  File Name : UncaughtException.java
+ * */
+package exceptions;
+
+public class UncaughtException {
+}
