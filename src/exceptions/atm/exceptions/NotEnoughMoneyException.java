13ab23fd5911ab9811836f6ee0e4800e3e16d321
dbc2201 <divyanshb22@gmail.com>
2020-09-09 15:25:37 +0530

    create NotEnoughMoneyException.java

---
+++ src/exceptions/atm/exceptions/NotEnoughMoneyException.java
@@ -0,0 +1,10 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 09/09/20
+ *  Time: 3:25 PM
+ *  File Name : NotEnoughMoneyException.java
+ * */
+package exceptions.atm.exceptions;
+
+public class NotEnoughMoneyException {
+}
