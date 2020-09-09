6cdba7632a25c11e90204dcdd83258e25f79ffdb
dbc2201 <divyanshb22@gmail.com>
2020-09-09 15:19:38 +0530

    create Interface ATM

---
+++ src/exceptions/atm/ATM.java
@@ -0,0 +1,10 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 09/09/20
+ *  Time: 3:19 PM
+ *  File Name : ATM.java
+ * */
+package exceptions.atm;
+
+public interface ATM {
+}
