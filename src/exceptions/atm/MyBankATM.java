1c728d02ab4b47acd9b67852bb0c6c83a89d7da6
dbc2201 <divyanshb22@gmail.com>
2020-09-09 15:21:20 +0530

    create MyBankATM.java

---
+++ src/exceptions/atm/MyBankATM.java
@@ -0,0 +1,10 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 09/09/20
+ *  Time: 3:21 PM
+ *  File Name : MyBankATM.java
+ * */
+package exceptions.atm;
+
+public class MyBankATM {
+}
