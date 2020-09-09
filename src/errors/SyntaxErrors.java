11f316eb34da8bbc5bc85fc74483fe1d70795a2b
dbc2201 <divyanshb22@gmail.com>
2020-09-09 10:13:53 +0530

    create SyntaxErrors.java

---
+++ src/errors/SyntaxErrors.java
@@ -0,0 +1,10 @@
+/*  Created by IntelliJ IDEA.
+ *  User: Divyansh Bhardwaj (dbc2201)
+ *  Date: 09/09/20
+ *  Time: 10:13 AM
+ *  File Name : SyntaxErrors.java
+ * */
+package errors;
+
+public class SyntaxErrors {
+}
